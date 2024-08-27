65cfb4ae0b4715b2738dc0a84bf094af1fc18256
Shengjie Xu <shengjiex98@gmail.com>
2024-08-27 12:15:28 -0400

    Add documentations

--- src/sim.jl
+++ src/sim.jl
@@ -15,7 +15,12 @@ const SYS = let
 end
 const K = lqr(Discrete, SYS.A, SYS.B, I, I)
 
-function sim(x::Vector{<:Real}, hit::Bool)
+"""
+    sim(x, hit)
+
+Simulate the plant's dynamics for one step, according to whether the deadline is met (`hit=true`) or missed (`hit=false`)
+"""
+function sim(x::Vector{<:Real}, hit::Bool)::Vector{Float64}
     length(x) == SYS.nx || error("Dimensions of the state `x` must match dimensions of the system")
     if hit
         return SYS.A * x - SYS.B * K * x
@@ -24,10 +29,20 @@ function sim(x::Vector{<:Real}, hit::Bool)
     end
 end
 
+"""
+    sim!(x, hit)
+
+Simulate the plant's dynamics for one step as in `sim()`, but *updates* the value in `x` in-place.
+"""
 function sim!(x::Vector{<:Real}, hit::Bool)
     x .= sim(x, hit)
 end
 
+"""
+    make_x0()
+
+A convenience function that returns an initial state with the correct dimensions for the plant.
+"""
 function make_x0()
     return repeat([1.], SYS.nx)
 end
